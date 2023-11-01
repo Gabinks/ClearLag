32368fbaf1ac3fef6a33e436443b535c1ab9dc88
Gabin Buignet <gabin.buignet@gmail.com>
2023-11-01 13:25:45 +0100

    Initial Commit

--- src/main/java/fr/gabinks/clearlag/Config.java
+++ src/main/java/fr/gabinks/clearlag/Config.java
@@ -4,31 +4,6 @@ import net.minecraftforge.common.ForgeConfigSpec;
 import org.apache.commons.lang3.tuple.Pair;
 
 public class Config {
-    /*private static ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
-    public static final General GENERAL = new General(BUILDER);
-    public static final ForgeConfigSpec spec = BUILDER.build();
-    public static class General{
-        public final ForgeConfigSpec.ConfigValue<Integer> TIME_BETWEEN_CLEARLAG;
-        public final ForgeConfigSpec.ConfigValue<String> MOB_FILTER;
-        public final ForgeConfigSpec.ConfigValue<String> CL_REMAINING;
-        public final ForgeConfigSpec.ConfigValue<String> CL_EXECUTED;
-        public General(ForgeConfigSpec.Builder builder){
-            builder.push("General");
-            TIME_BETWEEN_CLEARLAG = builder
-                    .comment("Change the time between every clearlag (in seconds)")
-                    .define("time_between_clearlag", 40);
-            MOB_FILTER = builder
-                    .comment("Add the mobs you want to clear Example : zombie,skeleton,creeper,wither")
-                    .define("mob_filter", "");
-            CL_REMAINING = builder
-                    .comment("Customize the message for the ClearLag. {timeRemaining} is the seconds before ClearLag execution.")
-                    .define("clearlag_remaining_time_message", "ClearLag is comming in {timeRemaining} seconds");
-            CL_EXECUTED = builder
-                    .comment("Customize the message for the ClearLag. {entities} is the number of entities removed.")
-                    .define("clearlag_executed_message", "ClearLag removed {entities} entities");
-            builder.pop();
-        }
-    }*/
     public static final ForgeConfigSpec COMMON_SPEC;
     public static final CommonConfig COMMON;
     static{
